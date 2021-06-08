a28c95dd64e36a5ab38e01ce5304c965325ee6d9
Pamella Gaiguer <sitpxg0@sonichealth.com.au>
2021-06-08 15:50:17 +1000

    starting services page

---
+++ src/app/API.ts
@@ -0,0 +1,60 @@
+import { getLocaleDateTimeFormat } from '@angular/common';
+
+export const servicesMockup = [
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  },
+  {
+    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    title: 'Curso de Interpretacao',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    date: getLocaleDateTimeFormat
+  }
+];
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { servicesMockup } from '../../API';
 
 @Component({
   selector: 'app-services',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
+  services = servicesMockup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
