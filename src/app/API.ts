50a5b70a7b6d6a826c6c7f535f5ea6765255a038
Pamella Gaiguer <sitpxg0@sonichealth.com.au>
2021-06-08 16:02:04 +1000

    starting website to prepare to receive api

--- src/app/API.ts
+++ src/app/API.ts
@@ -3,58 +3,51 @@ import { getLocaleDateTimeFormat } from '@angular/common';
 export const servicesMockup = [
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
+    tab_name: 'interp',
     title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   },
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    tab_name: 'circo',
+    title: 'Curso de Circo',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   },
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    tab_name: 'fotog',
+    title: 'Material Fotografico',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   },
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    tab_name: 'event',
+    title: 'Eventos',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   },
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    tab_name: 'agenc',
+    title: 'Agenciamento',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   },
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    tab_name: 'video',
+    title: 'Videobook',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   },
   {
     icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
-    date: getLocaleDateTimeFormat
-  },
-  {
-    icon: 'https://dummyimage.com/80x80/888888/fff.png&text=icon',
-    title: 'Curso de Interpretacao',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \n Aut dolorum culpa eaque deleniti similique totam accusamus reiciendis dicta maiores, fugit natus beatae.',
+    tab_name: 'conv',
+    title: 'Convenios',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     date: getLocaleDateTimeFormat
   }
 ];
