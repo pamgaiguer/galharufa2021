303f2f445ff7f3613dbd71a45df2ceda38257e3c
Leonardo <leonardo.aa88@gmail.com>
2021-10-30 03:07:24 -0300

    Update admin panel visualization

--- galharufa2021_restful_api/casting/admin.py
+++ galharufa2021_restful_api/casting/admin.py
@@ -4,10 +4,22 @@ from .models import Endereco, DadosBancarios, Pessoa
 # Register your models here.
 
 
+class EnderecoAdmin(admin.ModelAdmin):
+    list_filter = ("cidade", "estado")
+    list_display = ("logradouro", "numero", "bairro", "cidade", "estado")
+
+
+class DadosBancariosAdmin(admin.ModelAdmin):
+    list_filter = ("banco",)
+    list_display = ("banco", "agencia", "conta")
+
+
 class PessoaAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
+    list_filter = ("tipo",)
+    list_display = ("nome_completo", "nome_artistico", "sexo", "tipo",)
 
 
-admin.site.register(Endereco)
-admin.site.register(DadosBancarios)
+admin.site.register(Endereco, EnderecoAdmin)
+admin.site.register(DadosBancarios, DadosBancariosAdmin)
 admin.site.register(Pessoa, PessoaAdmin)
